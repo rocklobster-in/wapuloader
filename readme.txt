580b71206a8ce9ade742bf14e70ddd56f242a725
Takayuki Miyoshi <takayukister@gmail.com>
2016-12-10 13:53:46 +0900

    Add files

---
+++ readme.txt
@@ -0,0 +1,12 @@
+=== Wapuloader for Contact Form 7 ===
+Contributors: takayukister
+Tags: contact form, wapuu
+Requires at least: 4.5
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Replaces Contact Form 7's Ajax-loader (spinner) image with Wapuu.
+
+== Description ==
+
+This plugin works with Contact Form 7 4.6 or higher. Also, since it uses relatively modern techniques like SVG or CSS animation, some legacy browsers won't render the Ajax-loader correctly.
---
+++ wapuloader.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: Wapuloader for Contact Form 7
+Plugin URI: http://contactform7.com/
+Description: Replaces Contact Form 7's Ajax-loader (spinner) image with Wapuu.
+Author: Takayuki Miyoshi
+Author URI: http://ideasilo.wordpress.com/
+Version: 0.71-gold
+*/
+
+define( 'WAPULOADER_VERSION', '0.71-gold' );
+
+define( 'WAPULOADER_PLUGIN', __FILE__ );
+
+add_action( 'wp_enqueue_scripts', 'wapuloader_enqueue_scripts', 11 );
+
+function wapuloader_enqueue_scripts() {
+	wp_enqueue_style( 'wapuloader',
+		plugins_url( 'style.css', WAPULOADER_PLUGIN ),
+		array(), WAPULOADER_VERSION, 'screen' );
+
+	if ( is_rtl() ) {
+		wp_enqueue_style( 'wapuloader-rtl',
+			plugins_url( 'style-rtl.css', WAPULOADER_PLUGIN ),
+			array( 'wapuloader' ), WAPULOADER_VERSION, 'screen' );
+	}
+}
