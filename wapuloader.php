a5d7ccaf950f9e1e32550c178ac2e1ace9d41842
Takayuki Miyoshi <takayukister@gmail.com>
2022-11-12 13:25:05 +0900

    Update wapuloader.php

--- wapuloader.php
+++ wapuloader.php
@@ -5,55 +5,38 @@ Plugin URI: https://contactform7.com/donate/
 Description: Wapuloader replaces Contact Form 7's spinner icon with a rolling Wapuu.
 Author: Takayuki Miyoshi
 Author URI: https://ideasilo.wordpress.com/
-Version: 0.72
+Version: 1.0-dev
 */
 
-define( 'WAPULOADER_VERSION', '0.72' );
+define( 'WAPULOADER_VERSION', '1.0-dev' );
 
 define( 'WAPULOADER_PLUGIN', __FILE__ );
 
-add_action( 'wp_enqueue_scripts', 'wapuloader_enqueue_scripts', 11 );
+
+add_action( 'wp_enqueue_scripts', 'wapuloader_enqueue_scripts', 11, 0 );
 
 function wapuloader_enqueue_scripts() {
-	if ( ! defined( 'WPCF7_VERSION' ) ) {
-		return;
+
+	wp_register_style(
+		'wapuloader',
+		plugins_url( 'style.css', WAPULOADER_PLUGIN ),
+		array( 'contact-form-7' ),
+		WAPULOADER_VERSION,
+		'screen'
+	);
+
+	wp_register_style(
+		'wapuloader-rtl',
+		plugins_url( 'style-rtl.css', WAPULOADER_PLUGIN ),
+		array( 'wapuloader' ),
+		WAPULOADER_VERSION,
+		'screen'
+	);
+
+	wp_enqueue_style( 'wapuloader' );
+	
+	if ( is_rtl() ) {
+		wp_enqueue_style( 'wapuloader-rtl' );
 	}
 
-	if ( version_compare( WPCF7_VERSION, '5.4-dev', '<' ) ) {
-		wp_enqueue_style(
-			'wapuloader',
-			plugins_url( 'style-old.css', WAPULOADER_PLUGIN ),
-			array(),
-			WAPULOADER_VERSION,
-			'screen'
-		);
-
-		if ( is_rtl() ) {
-			wp_enqueue_style(
-				'wapuloader-rtl',
-				plugins_url( 'style-rtl-old.css', WAPULOADER_PLUGIN ),
-				array( 'wapuloader' ),
-				WAPULOADER_VERSION,
-				'screen'
-			);
-		}
-	} else {
-		wp_enqueue_style(
-			'wapuloader',
-			plugins_url( 'style.css', WAPULOADER_PLUGIN ),
-			array(),
-			WAPULOADER_VERSION,
-			'screen'
-		);
-
-		if ( is_rtl() ) {
-			wp_enqueue_style(
-				'wapuloader-rtl',
-				plugins_url( 'style-rtl.css', WAPULOADER_PLUGIN ),
-				array( 'wapuloader' ),
-				WAPULOADER_VERSION,
-				'screen'
-			);
-		}
-	}
 }
